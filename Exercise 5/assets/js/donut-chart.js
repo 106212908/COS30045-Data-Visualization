f92a075a192477569aa3865f7af9e62a93cab7ad
106212908 <106212908@student.swin.edu.au>
2026-09-22 21:13:24 +0800

    Fix variable error

--- Exercise 5/assets/js/donut-chart.js
+++ Exercise 5/assets/js/donut-chart.js
@@ -1,6 +1,6 @@
 d3.csv("assets/data/Data_exercise_5.3.csv").then(data => {
     data.forEach(d => {
-        d.screensizeCategory = +d["Screensize_Category"]
+        d.screensizeCategory = d["Screensize_Category"]
         d.count = +d["Count"]
     })
 
